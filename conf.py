955bddea8a5f24d880b9ca00e6f492c29849d9d0
Raunak Das <it1811@cemk.ac.in>
2021-04-06 20:51:19 +0530

    v1.0

---
+++ conf.py
@@ -0,0 +1,8 @@
+bolt_api_key = "cdd8869a-b0c9-4556-8225-1d841bcb0694"
+device_id = "BOLT13168528"
+telegram_chat_id = "@wifiscout"
+telegram_bot_id = "bot1779073104:AAF0nJKbXjqXa05J-e4M83zwOx0pYZ71BKM"
+
+# For pingmaster
+frame_size = 5
+mul_factor = 5
