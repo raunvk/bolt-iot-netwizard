380d5bd02e10242bd94c670b8adf8fad7e371544
Raunak Das <it1811@cemk.ac.in>
2021-04-06 20:55:02 +0530

    v1.0

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
-bolt_api_key = "cdd8869a-b0c9-4556-8225-1d841bcb0694"
-device_id = "BOLT13168528"
-telegram_chat_id = "@wifiscout"
-telegram_bot_id = "bot1779073104:AAF0nJKbXjqXa05J-e4M83zwOx0pYZ71BKM"
+bolt_api_key = "XXXX"         # Replace XXXX with your Bolt CLoud API Key
+device_id = "XXXX"            # Replace XXXX with your Bolt IoT Device ID
+telegram_chat_id = "@XXXX"    # Replace XXXX with your Telegram Channel ID
+telegram_bot_id = "botXXXX"   # Replace with Bot Token ID of your Telegram Bot
 
-# For pingmaster
+# For pingmaster.py
 frame_size = 5
 mul_factor = 5
