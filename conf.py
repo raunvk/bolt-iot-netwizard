3aa1c17ee9cf71322854842ff72214a91ba36689
Raunak Das <it1811@cemk.ac.in>
2021-04-08 23:32:22 +0530

    v1.0

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 bolt_api_key = "XXXX"         # Replace XXXX with your Bolt CLoud API Key
 device_id = "XXXX"            # Replace XXXX with your Bolt IoT Device ID
 telegram_chat_id = "@XXXX"    # Replace XXXX with your Telegram Channel ID
-telegram_bot_id = "botXXXX"   # Replace with Bot Token ID of your Telegram Bot
+telegram_bot_id = "botXXXX"   # Replace XXXX with Bot Token ID of your Telegram Bot
 
 # For Z-Score analysis in pingmaster.py
 frame_size = 5                # Edit frame_size (higher value denotes more test cases)
